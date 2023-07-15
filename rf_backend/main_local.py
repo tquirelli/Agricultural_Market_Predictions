b3d66bf5738f4a8e4a6c3a27baf0813a558e2625
Tomas Quirelli <tomasquirelli@gmail.com>
2023-07-15 13:14:30 -0300

    estructura funcional final modelo Random Forest para predecir M+1

---
+++ rf_backend/main_local.py
@@ -0,0 +1,130 @@
+import pandas as pd
+import numpy as np
+import pickle
+import os
+from sklearn.model_selection import cross_val_score
+from rf_backend.preprocesador_rf import all_preprocessor
+from rf_backend.RandomForest import initialize_rf, mape_score
+from dateutil.relativedelta import relativedelta
+
+LOCAL_PATH = 'Agricultural_data/consolidado_final.csv'
+script_path = os.path.dirname(__file__)
+csv_path = os.path.join(script_path, "..", LOCAL_PATH)
+consolidado = pd.read_csv(LOCAL_PATH,parse_dates=["date"],header=0)
+
+def get_new_date(N_month_predict):
+    Current_date = pd.Timestamp.now()
+    New_date = Current_date + relativedelta(months=N_month_predict)
+    month = New_date.month
+    year = New_date.year
+    print("Mes a predecir:", month)
+    print("Año a predecir:", year)
+    return int(month), int(year)
+
+def get_current_date():
+    Current_date = pd.Timestamp.now()
+    month = Current_date.month
+    year = Current_date.year
+    return int(month), int(year)
+
+def input_usuario(N_month_predict:int, consolidado:pd.DataFrame = consolidado):
+    # verificar que se encuentre en la base de datos
+    df = all_preprocessor(consolidado) # preprocesando el dataframe completo
+    month_date, year_date = get_new_date(N_month_predict) # obteniendo mes y año actual
+    #date = pd.to_datetime(f'{year_date}-{month_date:02d}-01')
+    # Sumar los meses a la fecha actual utilizando relativedelta
+
+    date = f'{year_date}-{month_date}'
+    print(date)
+    if date in df.index:
+        filtered_df = df[df.index == date]
+        print(f"✅ data is valid")
+        print(filtered_df.index)
+        return filtered_df, N_month_predict
+    else:
+        print(f"❌ data is not valid, please try again")
+        return "❌ data is not valid, please try again"
+
+def train_rf(consolidado:pd.DataFrame = consolidado) -> pd.DataFrame:
+    df = all_preprocessor(consolidado)
+
+    #Scores = {}
+    #Results = {}
+    #User_results = {}
+
+    features = [
+    "ewma_2", "ewma_3", "ewma_6", "ma_2", "ma_3", "ma_6", "x_1",
+    "x_2", "x_3", "x_4", "x_5", "x_6", "x_7", "x_8", "x_9", "x_10", "x_11", "x_12",
+    "usa_prod", "SP500", "real_interest_rate"
+    ]
+
+    new_features = df[features].fillna(0).values
+    mo,ye=get_current_date()
+    # create df_train and df_test
+    df_train = df[df.index<=f"{ye}-{mo}"]
+    #df_test = df[df.index>="2016-07-01"]
+
+    # Keep track of test_indexes
+    #test_indexes = np.arange(len(df_train), len(df_train)+len(df_test))
+    #test_indexes
+
+    model_init = initialize_rf()
+
+    #score = cross_val_score(model_init,X,y_log,cv=5,scoring=mape_score(),n_jobs=-1).mean()
+
+    #initialize_train_model, mape_score
+    #Scores['cross_val_score_{}'.format(N_month_predict)] = score
+
+    X_train=df_train[["usa_prod","x_1","x_2","x_3","x_4","x_5","x_6","x_7","x_8","x_9","x_10","x_11","x_12","SP500"]]
+    y_train=df_train["price_soybean"]
+
+    model=model_init.fit(X_train, y_train)
+    save_rf_model(model)
+
+    return model
+
+def save_rf_model(model):
+    model_path = os.path.join(script_path, 'model_rf')
+    with open(model_path, 'wb') as archivo:
+        pickle.dump(model, archivo)
+    print("Modelo RF guardado ✅.")
+
+###
+def predict_rf(filtered_df, N_month_predict):
+    #if meses --> cargar modelo
+    User_results = {}
+    try:
+        model_path = os.path.join(script_path, 'model_rf')
+        with open(model_path, 'rb') as archivo:
+            load_model = pickle.load(archivo)
+        print("Modelo RF cargado exitosamente ✅.")
+        predict_features=filtered_df[["usa_prod","x_1","x_2","x_3","x_4","x_5","x_6","x_7","x_8","x_9","x_10","x_11","x_12","SP500"]] #tendrían que ser las features solo del Mes que quiero predecir!
+        User_results['y_predict_{}'.format(N_month_predict)] = load_model.predict(predict_features)
+        print(predict_features)
+        price_soybean_result = User_results['y_predict_{}'.format(N_month_predict)][0]
+        print("El precio de la soja dentro de {} meses será de $: ".format(N_month_predict),price_soybean_result)
+        return price_soybean_result
+
+    except FileNotFoundError:
+        train_rf(consolidado)
+
+###
+
+if __name__ == '__main__':
+    try:
+        LOCAL_PATH = 'Agricultural_data/consolidado_final.csv'
+        script_path = os.path.dirname(__file__)
+        csv_path = os.path.join(script_path, "..", LOCAL_PATH)
+        consolidado = pd.read_csv(csv_path,parse_dates=["date"],header=0)
+        N_month_predict = 1
+        filtered_df, N_month_predict = input_usuario(N_month_predict, consolidado)
+        predict_rf(filtered_df, N_month_predict)
+
+    except:
+        import sys
+        import traceback
+
+        import ipdb
+        extype, value, tb = sys.exc_info()
+        traceback.print_exc()
+        ipdb.post_mortem(tb)
---
+++ rf_backend/preprocesador_rf.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import numpy as np
+
+def all_preprocessor(df) -> pd.DataFrame:
+    # Select the desired columns
+    df = df.rename(columns={'SOYBEANS - USA PRODUCTION [mTons]': 'usa_prod'})
+    df = df[['date', 'price_soybean', 'real_interest_rate', 'SP500', 'usa_prod']]
+
+    # Convert 'date' column to datetime
+    df['date'] = pd.to_datetime(df['date']).dt.to_period('M')
+    df.set_index(["date"],inplace=True)
+    # Add lagged values
+    df2 = df.copy()
+    for i in range(1, 13):
+        df2[f'x_{i}'] = df["price_soybean"].shift(i)
+
+    #Drop nan by eliminating first 12 months
+    df2=df2[df2.index >= "1989-07-01"]
+
+
+    # Extract the trend using a well-chosen moving average
+    df2['ma_6'] = df2['price_soybean'].shift(1).rolling(window=6).mean()
+    df2['ma_3'] = df2['price_soybean'].shift(1).rolling(window=3).mean()
+    df2['ma_2'] = df2['price_soybean'].shift(1).rolling(window=2).mean()
+
+    # Moving average with halflife
+    df2["ewma_2"] = df2["price_soybean"].shift(1).ewm(halflife=2).mean()
+    df2["ewma_3"] = df2["price_soybean"].shift(1).ewm(halflife=3).mean()
+    df2["ewma_6"] = df2["price_soybean"].shift(1).ewm(halflife=6).mean()
+
+    # To save the dataframe in the dataser folder
+    df2.to_csv("preprocessed_data_rf.csv", index=True)
+
+    return df2
