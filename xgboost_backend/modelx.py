d470e9b5a17f6d140901d92fdef3d906e52c0653
felipe posse <fposse.mde@gmail.com>
2023-07-15 11:23:23 -0300

    6 meses in

--- xgboost_backend/modelx.py
+++ xgboost_backend/modelx.py
@@ -26,6 +26,15 @@ def predict_price(time_horizon):
 
 
     elif time_horizon == "6 Months":
-        return "Welcome to the 6 Months prediction"
+
+        LOCAL_PATH = 'Agricultural_data/consolidado_final1.csv'
+        script_path = os.path.dirname(__file__)
+        csv_path = os.path.join(script_path, "..", LOCAL_PATH)
+        consolidado = pd.read_csv(csv_path)
+        N_month_predict = 6
+        filtered_df, N_month_predict = input_usuario(N_month_predict, consolidado)
+        result = predict_xgboost(filtered_df, N_month_predict)
+
+        return result
     else:
         return ""
